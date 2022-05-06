2e4715aa625e94966a9e31a811a1aa92c87cb759
Jon Phillipo <jdphil@gmail.com>
2022-05-06 06:14:44 -0500

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents Healthy Diner')
+streamlit.title('My Mom\'s Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
